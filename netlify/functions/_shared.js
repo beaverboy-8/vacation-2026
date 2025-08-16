210e857718ed4136db6ced080a018d7c1901ef0b
beaverboy-8 <skylerkhunter@gmail.com>
2025-08-16 12:21:53 -0400

    Update _shared.js

--- netlify/functions/_shared.js
+++ netlify/functions/_shared.js
@@ -1,5 +1,5 @@
 export const STORE_NAME = 'vacay2026';
-export const CAP = 12;
+export const CAP = 13;
 export function jsonResponse(obj, status = 200, headers = {}) {
   return new Response(JSON.stringify(obj), {
     status,
